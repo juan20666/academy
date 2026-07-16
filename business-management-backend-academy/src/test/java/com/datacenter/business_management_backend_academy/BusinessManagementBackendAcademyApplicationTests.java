c7ffd48e5a62d35ab525de7248b3ff8290f6cfd6
juan.correa <juan.correa@dcsas.com.co>
2026-07-16 09:34:14 -0500

    first commit

---
+++ business-management-backend-academy/src/test/java/com/datacenter/business_management_backend_academy/BusinessManagementBackendAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datacenter.business_management_backend_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementBackendAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
